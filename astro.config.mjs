71e93ea3bd53dc9d0c2c71a6fc650364a321bd71
Ryan Furrer <ryandotfurrer@gmail.com>
2025-02-12 17:26:23 -0500

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+
+import vercel from '@astrojs/vercel';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  adapter: vercel(),
+});
---
+++ todo.md
@@ -0,0 +1,33 @@
+# Tasks
+
+## CSS
+
+- [ ] Add base colors
+- [ ] Add typography styles
+- [ ] Add a component styles
+
+## Admin
+
+- [ ] Setup head component
+
+## Components
+
+### Astro Components
+
+- Link
+- HGroup
+- Card
+- Header
+- HGroup
+- Footer
+
+### React Components
+
+- Mobile Nav
+- Button
+- Input
+- Select
+- Checkbox
+- Radio
+- Switch
+- ThemeToggle
