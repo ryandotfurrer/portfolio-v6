88ccdac25aa607721189c27921052aa24f71a1c0
Ryan Furrer <ryandotfurrer@gmail.com>
2025-02-24 10:03:30 -0500

    Refactor Navbar and MobileMenu components for improved styling and consistency

--- src/components/react/MobileMenu.tsx
+++ src/components/react/MobileMenu.tsx
@@ -7,47 +7,47 @@ export default function MobileMenu() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className='md:hidden'>
+    <div className="md:hidden">
       <button
         onClick={() => setIsOpen(!isOpen)}
-        className='cursor-pointer relative z-50'
-        aria-label='Toggle mobile menu'
+        className="relative z-50 cursor-pointer"
+        aria-label="Toggle mobile menu"
       >
         {isOpen ? 'close' : 'menu'}
       </button>
 
       <div
-        className={`w-[91.8%] fixed top-21.2 right-4 z-40 p-4 rounded-b-lg border border-border border-t-0 text-right bg-card transform transition-all duration-300 origin-top ${
+        className={`top-21.2 border-border bg-card fixed right-4 z-40 w-[91.9%] origin-top transform rounded-b-lg border border-t-0 p-4 text-right transition-all duration-300 ${
           isOpen ? 'scale-y-100' : 'scale-y-0'
         }`}
       >
-        <ul className='flex flex-col gap-8 pt-12'>
+        <ul className="flex flex-col gap-8 pt-12">
           {NavLinks.map((link, index) => (
             <li
               key={link.href}
-              className={`transform blur-[8px] opacity-0 transition-all duration-300 ${
-                isOpen ? 'blur-none opacity-100' : ''
+              className={`transform opacity-0 blur-[8px] transition-all duration-300 ${
+                isOpen ? 'opacity-100 blur-none' : ''
               }`}
               style={{
                 transitionDelay: isOpen ? `${index * 100}ms` : '0ms',
               }}
             >
-              <a href={link.href} className='block'>
+              <a href={link.href} className="block">
                 {link.label}
               </a>
             </li>
           ))}
           <li
-            className={`transform blur-[8px] opacity-0 transition-all duration-300 ${
-              isOpen ? 'blur-none opacity-100' : ''
+            className={`transform opacity-0 blur-[8px] transition-all duration-300 ${
+              isOpen ? 'opacity-100 blur-none' : ''
             }`}
             style={{
               transitionDelay: isOpen ? `${NavLinks.length * 100}ms` : '0ms',
             }}
           >
             <a
-              href='/contact'
-              className='bg-primary text-primary-foreground hover:bg-primary/90 h-9 rounded-md px-3 inline-flex items-center justify-center'
+              href="/contact"
+              className="bg-primary text-primary-foreground hover:bg-primary/90 inline-flex h-9 items-center justify-center rounded-md px-3"
             >
               contact
             </a>
