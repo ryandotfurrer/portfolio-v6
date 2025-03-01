a97dfa0049814fab805616843f3fce6739dc07f2
Ryan Furrer <ryandotfurrer@gmail.com>
2025-03-01 12:50:10 -0500

    Update footer component to include sitemap, social links, and newsletter subscription form

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://ryanfurrer.com',
   integrations: [mdx(), sitemap(), react()],
   vite: {
     plugins: [tailwindcss()],
