dccb7cacbc26c9284b09db868717d5ae00a9ad34
Ryan Furrer <ryandotfurrer@gmail.com>
2025-03-18 20:50:18 -0400

    Add experimental contentIntellisense to Astro config and update package dependencies to latest versions, including @astrojs/mdx, @astrojs/sitemap, and others for improved performance and stability.

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,9 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+  contentIntellisense: true,
+},
   site: 'https://ryanfurrer.com',
   integrations: [mdx(), sitemap(), react()],
   vite: {
