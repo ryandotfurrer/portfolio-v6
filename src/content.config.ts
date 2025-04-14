d0d4b05150af60ef4a4557af1206cdb277506b16
Ryan Furrer <ryandotfurrer@gmail.com>
2025-04-14 13:16:53 -0400

    Remove unused makeRequest function from content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -30,24 +30,6 @@ const blog = defineCollection({
   }),
 });
 
-const makeRequest = (url: string): Promise<any> => {
-  return new Promise((resolve, reject) => {
-    https
-      .get(url, (res) => {
-        let data = '';
-        res.on('data', (chunk) => (data += chunk));
-        res.on('end', () => {
-          try {
-            resolve(JSON.parse(data));
-          } catch (e) {
-            reject(e);
-          }
-        });
-      })
-      .on('error', reject);
-  });
-};
-
 const newsletter = defineCollection({
   loader: async () => {
     try {
