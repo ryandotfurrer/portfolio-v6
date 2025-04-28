62558b731d48ec67c9211b0affd785e6ceb2187f
Ryan Furrer <ryandotfurrer@gmail.com>
2025-04-28 16:04:23 -0400

    Update SITE_DESCRIPTION in consts.ts and clean up BaseHead.astro by removing default description assignment.

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Ryan Furrer';
-export const SITE_DESCRIPTION = 'Frontend Engineer';
+export const SITE_DESCRIPTION =
+  'Frontend engineer that loves to make sh*t look good. I am also a blogger, streamer, and public speaker.';
